ac9d58901cbc79f10e97c7b12d2622c4b374a3a9
trent-001 <trent@doras.to>
2025-07-10 01:26:04 +1000

    init

--- packages/backend/auth.ts
+++ packages/backend/auth.ts
@@ -1,8 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "../db/index"; // your drizzle instance
-import * as schema from "../db/schema/auth-schema";
-
+import * as schema from "db/schema/auth-schema";
+import { db } from "db";
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg", // or "mysql", "sqlite"
